3b78477b8e834a7d06cdbf8f71042c18ae886653
Kitti002 <59986706+Kitti002@users.noreply.github.com>
2022-03-04 11:34:52 +0700

    Update name.js

--- frontend/src/components/name.js
+++ frontend/src/components/name.js
@@ -1,8 +1,8 @@
 function MyName() {
   return (
     <div>
-      <div>กิตติ ธรรมธร</div>
-      <div>610112418020</div>
+      <div>Kitti Thammathonr</div>
+      <div>610112418002</div>
     </div>
   );
 }
